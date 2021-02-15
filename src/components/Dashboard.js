0695959729b322681e04a182aea484d8f67a93aa
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-02-15 17:07:55 +0200

    Adds dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,9 +4,20 @@ import ProjectItem from "./Project/ProjectItem";
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
-        <ProjectItem />
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+
+              <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                Create a Project
+              </a>
+
+              <hr></hr>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
