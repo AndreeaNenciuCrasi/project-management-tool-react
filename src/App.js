7db94343df7f9a01f8ee551b8473bfc6c7afa20a
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-02-16 14:20:30 +0200

    Adds component CreateProjectButton

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Dashboard />
+      </div>
+    </Router>
   );
 }
 
