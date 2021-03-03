38a0dc215c2f024c07b77a26d4defc8a03ed992d
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-03-03 16:30:12 +0200

    Adds action for add project task

---
+++ src/actions/backlogActions.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const addProjectTask = (backlog_id, project_task, history) => async dispatch => {
+    await axios.post(`/api/backlog/${backlog_id}`, project_task);
+}
