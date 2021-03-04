68cba05e76f2678c59969a9fa1054406df38b0ef
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-03-04 19:32:28 +0200

    Lists tasks by status in the 3 columns todo, in progress and done

--- src/components/ProjectBoard/Backlog.js
+++ src/components/ProjectBoard/Backlog.js
@@ -7,6 +7,25 @@ import ProjectTask from './ProjectTasks/ProjectTask'
          const tasks = project_tasks_prop.map(project_task => (
              <ProjectTask key={project_task.id} project_task={project_task} />
          ));
+
+         let todoItems = [];
+         let inProgressItems = [];
+         let doneItems = [];
+
+         for (let i = 0; i < tasks.length; i++){
+            //  console.log(tasks[i]);
+             if (tasks[i].props.project_task.status === "TO_DO") {
+                 todoItems.push(tasks[i]);
+             }
+             if (tasks[i].props.project_task.status === "IN_PROGRESS") {
+                inProgressItems.push(tasks[i]);
+             }
+             if (tasks[i].props.project_task.status === "DONE") {
+                doneItems.push(tasks[i]);
+            }
+         }
+
+
         return (
             <div className="container">  
                 <div className="row">
@@ -16,24 +35,25 @@ import ProjectTask from './ProjectTasks/ProjectTask'
                                 <h3>TO DO</h3>
                             </div>
                         </div>       
-                    {tasks}       
+                    {todoItems}       
                     </div>
-                            <div className="col-md-4">
-                                <div className="card text-center mb-2">
-                                    <div className="card-header bg-primary text-white">
-                                        <h3>In Progress</h3>
-                                    </div>
-                                </div>
-                                
+                    <div className="col-md-4">
+                        <div className="card text-center mb-2">
+                            <div className="card-header bg-primary text-white">
+                                <h3>In Progress</h3>
                             </div>
-                            <div className="col-md-4">
-                                <div className="card text-center mb-2">
-                                    <div className="card-header bg-success text-white">
-                                        <h3>Done</h3>
-                                    </div>
-                                </div>
-                                
+                        </div>
+                        {inProgressItems} 
+                    </div>
+
+                    <div className="col-md-4">
+                        <div className="card text-center mb-2">
+                            <div className="card-header bg-success text-white">
+                                <h3>Done</h3>
                             </div>
+                        </div>
+                        {doneItems}   
+                    </div>
                         </div>
                     </div>
         )
