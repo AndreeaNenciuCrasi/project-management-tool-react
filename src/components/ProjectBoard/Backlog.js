97ff9dbca4a6ea3a97fe4fb128ffad34a00481d5
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-03-04 19:12:47 +0200

    Lists tasks in the to do column

--- src/components/ProjectBoard/Backlog.js
+++ src/components/ProjectBoard/Backlog.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react'
 import ProjectTask from './ProjectTasks/ProjectTask'
 
  class Backlog extends Component {
-    render() {
+     render() {
+         const { project_tasks_prop } = this.props;
+         const tasks = project_tasks_prop.map(project_task => (
+             <ProjectTask key={project_task.id} project_task={project_task} />
+         ));
         return (
-            <div className="container">
+            <div className="container">  
                 <div className="row">
                     <div className="col-md-4">
                         <div className="card text-center mb-2">
@@ -12,7 +16,7 @@ import ProjectTask from './ProjectTasks/ProjectTask'
                                 <h3>TO DO</h3>
                             </div>
                         </div>       
-                    <ProjectTask/>        
+                    {tasks}       
                     </div>
                             <div className="col-md-4">
                                 <div className="card text-center mb-2">
--- src/components/ProjectBoard/ProjectTasks/ProjectTask.js
+++ src/components/ProjectBoard/ProjectTasks/ProjectTask.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react'
 
  class ProjectTask extends Component {
-    render() {
+     render() {
+         const { project_task } = this.props;
         return (
             <div className="card mb-1 bg-light">
 
-                                    <div className="card-header text-primary">
-                                        ID: projectSequence -- Priority: priorityString
-                                    </div>
-                                    <div className="card-body bg-light">
-                                        <h5 className="card-title">project_task.summary</h5>
-                                        <p className="card-text text-truncate ">
-                                            project_task.acceptanceCriteria
-                                        </p>
-                                        <a href="#" className="btn btn-primary">
+                <div className="card-header text-primary">
+                ID: {project_task.projectSequence} -- Priority: {project_task.priorityString}
+                </div>
+                <div className="card-body bg-light">
+                    <h5 className="card-title">{project_task.summary}</h5>
+                        <p className="card-text text-truncate ">
+                        {project_task.acceptanceCriteria}
+                        </p>
+                        <a href="#" className="btn btn-primary">
                                             View / Update
-                                        </a>
+                        </a>
 
-                                        <button className="btn btn-danger ml-4">
+                        <button className="btn btn-danger ml-4">
                                             Delete
-                                        </button>
-                                    </div>
-                                </div>
+                        </button>
+                </div>
+            </div>
         )
     }
 }
