af69a4595769f776f91592bd433551bcc70012b1
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-03-29 16:45:41 +0300

    Gets logged user into store

--- src/components/UserManagement/Login.js
+++ src/components/UserManagement/Login.js
@@ -1,19 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import classnames from "classnames";
+import {login} from "../../actions/securityActions";
 
 class Login extends Component {
+    constructor(){
+        super();
+        this.state = {
+            username: "",
+            password: "",
+            errors: {}
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+      }
+
+      handleSubmit(e) {
+        e.preventDefault();
+        const loginRequest = {
+            username: this.state.username,
+            password: this.state.password
+        };
+        this.props.login(loginRequest, this.props.history);
+      }
     render() {
+        const {errors} = this.state;
         return (
             <div className="login">
         <div className="container">
             <div className="row">
                 <div className="col-md-8 m-auto">
                     <h1 className="display-4 text-center">Log In</h1>
-                    <form action="dashboard.html">
+                    <form onSubmit={this.handleSubmit}>
                         <div className="form-group">
-                            <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email"/>
+                            <input type="text" 
+                            className="form-control form-control-lg" 
+                            placeholder="Email Address" 
+                            name="username"
+                            value={this.state.username}
+                            onChange={this.handleChange}/>
                         </div>
                         <div className="form-group">
-                            <input type="password" className="form-control form-control-lg" placeholder="Password" name="password"/>
+                            <input type="password" 
+                            className="form-control form-control-lg" 
+                            placeholder="Password" 
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.handleChange}/>
                         </div>
                         <input type="submit" className="btn btn-info btn-block mt-4"/>
                     </form>
@@ -24,4 +61,13 @@ class Login extends Component {
         )
     }
 }
-export default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps =state =>({
+    security: state.security,
+    errors: state.errors
+})
+export default connect(mapStateToProps, {login})(Login);
