eb49388a4d47bd5f18a4167a72e61f9307ec26d9
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-05-20 12:34:32 +0300

    Adds videos to landing page

--- src/components/Layout/Landing.js
+++ src/components/Layout/Landing.js
@@ -18,7 +18,7 @@ class Landing extends Component {
             <div className="container">
                 <div className="row">
                     <div className="col-md-12 text-center">
-                        <h1 className="display-3 mb-4">Personal Project Management Tool</h1>
+                        <h3 className="landingTitle">Personal Project Management Tool</h3>
                         <p className="lead">
                             Create your account to join active projects or start your own
                         </p>
@@ -29,6 +29,19 @@ class Landing extends Component {
                         <Link to="/login" className="btn btn-lg btn-secondary mr-2">
                             Login
                         </Link>
+                        <br></br>
+                        <br></br>
+        <div class="container">
+        <div class="row">
+            <div class="col order-last">
+            <iframe width="520" height="275" src="https://www.youtube.com/embed/EG16dFYK0gw?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>            </div>
+            <div class="col">
+            <iframe width="520" height="275" src="https://www.youtube.com/embed/w6uciYsaJv0?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>            </div>
+            <div class="col order-first">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/IqukiTGZ0AY?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>             </div>
+        </div>
+        </div>
+                        
                     </div>
                 </div>
             </div>
