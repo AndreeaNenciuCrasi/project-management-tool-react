279d234bdb9e1adcb7ded19992b6ff2a04111556
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-08-25 11:58:36 +0300

    adds changes to project cards

--- src/components/Project/ProjectItem.js
+++ src/components/Project/ProjectItem.js
@@ -62,35 +62,45 @@ class ProjectItem extends Component {
 
     return (
       <div className="container">
-        <div class="card">
+        <div className="card">
           <div id="triangle"></div>
           <h2>{project.projectName}</h2>
-          <p>{project.description}</p>
+          <div className="row">
+          <div className="col-6">
+            <div className="project-description">
+          <p><b>Description: </b>{project.description}</p>
           {project.start_date &&
-              <p>Start date: {project.start_date}</p>}
+              <p><b>Start date: </b>{project.start_date}</p>}
               
               {project.end_date &&
-              <p>End date: <span className="text-danger font-weight-bold">{project.end_date}</span></p>
+              <p><b>End date: </b><span className="text-danger font-weight-bold">{project.end_date}</span></p>
               }
-              <form onSubmit={this.handleSubmit}>
+              <form onSubmit={this.handleSubmit} className="">
                 <label>
-                Add TeamMate:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />        </label>
-                    <input type="submit" value="Submit" />
+                <b>Add: </b>
+                    <input type="text" value={this.state.value} onChange={this.handleChange} 
+                    placeholder="TeamMate"/>        </label>
+                    <input type="submit" value="Submit" className="btn text-light bg-info btn-sm ml-2" />
               </form>
-              <div className="col-md-4 d-none d-lg-block px-md-5 margin-card-buttons">
+              </div>
+          </div>
+          
+          <div class="col-6">
+          <div className="d-lg-block margin-card-buttons">
               <ul className="list-group project-buttons-set">
                 
                 <Link to={`/projectBoard/${project.projectIdentifier}`}>
                   <li className="list-group-item text-dark bg-transparent no-border">
-                  <button className="btn text-light bg-dark"><i className="fa fa-flag-checkered pr-1 card-buttons"> </i> Project Board</button>
+                  <button className="btn text-light bg-dark">
+                    <i className="fa fa-flag-checkered pr-1 card-buttons"> </i> Project Board</button>
                   </li>
                 </Link>
                 
                 
                 <Link to={`/updateProject/${project.projectIdentifier}`}>
                   <li className="list-group-item bg-transparent no-border">
-                  <button className="btn text-light bg-dark"><i className="fa fa-edit pr-1 card-buttons"> </i> Update Project</button>
+                  <button className="btn text-light bg-dark">
+                    <i className="fa fa-edit pr-1 card-buttons"> </i> Update Project</button>
                   </li>
                 </Link>
                 
@@ -102,27 +112,31 @@ class ProjectItem extends Component {
                     project.projectIdentifier
                   )}
                 >
-                  <button className="btn text-light bg-dark"><i className="fa fa-minus-circle pr-1"> </i> Delete Project</button>
+                  <button className="btn text-light bg-dark">
+                    <i className="fa fa-minus-circle pr-1"> </i> Delete Project</button>
                 </li>
 
                 <li className="list-group-item text-dark bg-transparent no-border ">
-                <div className="dropdown">
+                <div className="dropup">
                   <button onClick={this.handleClick} 
                     className="btn text-light bg-dark dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Teammates
                   </button>
                   <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  {this.state.teammates && 
-                      (this.state.teammates.map(item =>( 
-                      
-                        <a className="dropdown-item"  key={item.id} href="#">{item.username} <button onClick={(e)=>{this.handleDeleteTeammate(e, item.id, project.id)}} className="btn text-light bg-info btn-sml">x</button></a>
-                      )))
-                  }
+                      {this.state.teammates && 
+                          (this.state.teammates.map(item =>( 
+                            <a className="dropdown-item"  key={item.id} href="#">{item.username} <button onClick={(e)=>{this.handleDeleteTeammate(e, item.id, project.id)}} className="btn text-light bg-info btn-sml">x</button></a>
+                          )))
+                      }
                   </div>
                 </div>
                 </li>
               </ul>
             </div>
+          </div>
+          </div>
+          
+              
         </div>
         
       </div>
